56352b39f45298ff24385dd120ea9588d3a04e36
Roman <92088165+ncallie@users.noreply.github.com>
2022-05-16 20:04:33 +0300

    Add files via upload

---
+++ ApplicationContext/src/main/java/ru/ncallie/TestBean.java
@@ -0,0 +1,22 @@
+package ru.ncallie;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
