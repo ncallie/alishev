e92e0c02cb72f075cebbd1982189d81a8f57997d
Roman <92088165+ncallie@users.noreply.github.com>
2022-05-17 13:48:20 +0300

    ver3

---
+++ 01_IoC&DI/IoC_ver3/src/main/java/ru/ncallie/Program.java
@@ -0,0 +1,15 @@
+package ru.ncallie;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Program {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+        musicPlayer.PlayAllMusic();
+        context.close();
+    }
+}
